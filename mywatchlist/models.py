5989b834a9a8f66cd75f9a27640df3808642a3f3
Irfan Satya Hendrasto <irfan.satya@ui.ac.id>
2022-09-21 21:53:01 +0700

    ngerjain bonus

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class FilmWatchlist(models.Model):
-    watched = models.TextField()
+    watched = models.BooleanField()
     title = models.CharField(max_length=255)
     rating = models.IntegerField()
     release_date = models.TextField()
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from django.shortcuts import render
 from mywatchlist.models import FilmWatchlist
 from django.http import HttpResponse
@@ -6,9 +7,22 @@ from django.core import serializers
 # Create your views here.
 def show_watchlist(request):
     data_film_watchlist = FilmWatchlist.objects.all()
+    watched = 0
+    unwatched = 0
+    pesan = ""
+    for i in data_film_watchlist:
+        if i.watched == True:
+            watched+=1
+        else:
+            unwatched+=1
+    if watched >= unwatched:
+        pesan = "Selamat, kamu sudah banyak menonton!"
+    else:
+        pesan ="Wah, kamu masih sedikit menonton!"
     context = {
         'list_film': data_film_watchlist,
-        'nama': 'Ipang'
+        'nama': 'Ipang',
+        'pesan' : pesan
     }
     return render(request, "mywatchlist.html", context)
 
